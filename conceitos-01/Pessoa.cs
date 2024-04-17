bfcefac370ee2a5cde8918c7c09d6e9b352deb41
lucaskennari <lucas.oheisen@gmail.com>
2024-04-17 16:05:45 -0300

    adicionado conceito: class, atributos e metodos

---
+++ conceitos-01/Pessoa.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Pessoa 
+{
+       // Atributos
+       public string nome;
+       public int idade;
+
+       //Metodos
+       public void mensagem()
+       {
+              Console.WriteLine("Olá " + nome + " Você tem " + idade + " anos ");
+       }
+
+}
---
+++ conceitos-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace conceitos
+{
+       class Program 
+       {
+              static void Main(string[] args)
+              {
+                     //instanciar um objeto
+                     Pessoa obj = new Pessoa();
+                     obj.nome = "Lucas";
+                     obj.idade = 26;
+                     obj.mensagem();
+              }
+       }
+}
